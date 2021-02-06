d203e4b684b14968aec5da17a8dd39f71257153f
Dinesh Katariya <33573799+Thedineshk24@users.noreply.github.com>
2021-02-07 00:02:26 +0530

    today task is done :)

--- src/Requests.js
+++ src/Requests.js
@@ -1,10 +1,11 @@
-const API_KEY = process.env.APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
+// console.log(API_KEY);
 
 const requests = {
     fetchTrending : `/trending/all/week?api_key=${API_KEY}&Language=en-US`,
-    fetchNetflixOrignals : `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated : `/movie/top_rated?api_key=${API_KEY}&Language=en-US`,
-    fetchActionMovies : `/discover/movie?api_eky=${API_KEY}&with_genres=28`,
+    fetchActionMovies : `/discover/movie?api_key=${API_KEY}&with_genres=28`,
     fetchComedyMovies : `/discover/movie?api_key=${API_KEY}&with_genres=35`,
     fetchHorrorMovies : `/discover/movie?api_key=${API_KEY}&with_genres=27`,
     fetchRomanceMovies : `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
